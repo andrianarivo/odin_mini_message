e14af44d73c38cf14ea48f41e83b30d7ba431c13
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-09 20:33:51 +0300

    build requirements

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,32 @@ const express = require('express');
 
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+/* GET new page. */
+router.get('/new', (req, res) => {
+  res.render('new');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({added: new Date(), user: req.body.user, text: req.body.message});
+  res.redirect("/");
 });
 
 module.exports = router;
